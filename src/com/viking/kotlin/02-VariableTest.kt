ba26f7da6de6346f9d368415876ac114c99b36fb
Viking Den <vikingden7@gmail.com>
2017-07-02 15:45:25 +0800

    add variables

---
+++ src/com/viking/kotlin/02-VariableTest.kt
@@ -0,0 +1,15 @@
+package com.viking.kotlin
+
+/**
+ * Author : Viking Den <vikingden7@gmail.com>
+ * Date : 2017/7/2
+ */
+fun main(args: Array<String>){
+    val a: String = "Hello, Kotlin"
+    val i: Int = 3
+
+    val fp = 123.3
+
+    val unInited: Int = 3
+
+}
---
+++ src/com/viking/kotlin/03-ValAndVar.kt
@@ -0,0 +1,26 @@
+package com.viking.kotlin
+
+/**
+ * Author : Viking Den <vikingden7@gmail.com>
+ * Date : 2017/7/2
+ */
+fun main(args: Array<String>){
+    val a = 3
+    val b = 6
+    val content: String
+    if(a > b){
+        content = "Ok"
+    }else{
+        content = "Cancel"
+    }
+
+    println(content)
+
+    val list = arrayListOf("1", "2")
+    list.add("3")
+
+
+    val type = 5
+//    type = "test"
+
+}
